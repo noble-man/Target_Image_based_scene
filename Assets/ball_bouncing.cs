a8b6c094dbe8deef775d2ffc3309e173ef023cda
Patrick Mbenza <pat.mbenza@gmail.com>
2019-06-27 04:13:22 +0200

    first commit

---
+++ Assets/ball_bouncing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_bouncing : MonoBehaviour
+{
+    //private Vector3[] verticalPositions;
+    // Start is called before the first frame update
+
+   // public Vector3[] _positions;
+
+    //public int _positionNbr;
+
+    //public float _swapPerSecond;
+
+    void Start()
+    {
+        gameObject.name = "nba_bball";
+
+      //  _positions = new Vector3[_positionNbr];
+
+        /*for (int i = 0; i < _positions.Length; i++)
+        {
+            _positions[i] = new Vector3(0, Random.Range(0f, 10f), 0);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.SetPositionAndRotation(new Vector3(0,Time.time % 4 ,0),new Quaternion());
+       // gameObject.transform.rotation.Set(0, gameObject.transform.rotation.y + 5, 0,0);
+        //gameObject.transform.SetPositionAndRotation();
+        //gameObject.transform.SetPositionAndRotation(gameObject.transform.position,new Quaternion(0, gameObject.transform.rotation.y + 5, 0,0));
+       // transform.position = _positions[Mathf.FloorToInt(Time.time * _swapPerSecond) % _positions.Length];
+    }
+}
---
+++ Assets/ball_spinning.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_spinning : MonoBehaviour
+{
+ 
+    public int _positionNbr; //user can define number of positions
+    public Vector3[] _positions;
+
+    private void Awake()
+    {
+        _positions = new Vector3[_positionNbr];
+
+        for (int i = 0; i < _positions.Length; i++)
+        {
+            _positions[i] = new Vector3(0, Random.Range(0f, 360f), 0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.localPosition = Vector3.right * Time.time;
+        //transform.localRotation = new Quaternion(0, Time.time, 0, 0); //* Time.time;
+        transform.Rotate(0,2,0,Space.Self);
+    }
+}
